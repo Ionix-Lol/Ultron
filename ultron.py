bb632fe9e8730f3e48608bb50b2a675cf52c677f
KorryKatti <60970528+korrykatti@users.noreply.github.com>
2021-10-12 16:21:28 +0530

    Update ultron.py

--- ultron.py
+++ ultron.py
@@ -1,13 +1,34 @@
 import pyttsx3
 from gtts import gTTS
+import datetime
 
 
 
 engine = pyttsx3.init()
-engine.say("Hi this is working ");
-engine.setProperty('volume',1)
-engine.runAndWait()
+voices = engine.getProperty('voices')
+engine.setProperty('rate', 160)
+#print(voices)
+engine.setProperty('voice', voices[11].id)
 
 
 def speak(audio):
-    pass
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>= 0 and hour<12:
+        speak("Good Morning Mortal You Wont Be Alive For Long")
+
+    elif hour>=12 and hour<18:
+        speak("Pretty good afternoon go to sleep till i review the human race")
+    
+    else:
+        speak("Good Evening go to play football or whatever the hell you like ")
+
+    speak("HEy i am ultron i had cut thanos into two")
+
+
+if __name__ == "__main__":
+    wishMe()
+    
