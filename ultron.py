beed3cb8af32cea90f5fd4d85ae490e5105a0386
KorryKatti <60970528+korrykatti@users.noreply.github.com>
2021-10-19 13:37:24 +0530

    Update ultron.py

--- ultron.py
+++ ultron.py
@@ -1,6 +1,15 @@
+''' 
+Coded by Korrykatti aka Abhinav 
+'''
+
+
+
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices =engine.getProperty('')
 try:
     with sr.Microphone() as source:
         print('listening now')
